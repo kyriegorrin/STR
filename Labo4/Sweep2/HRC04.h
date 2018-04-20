594e304e50ecc79dba7a7046a9f990b3ba9d0d24
Ricardo Zarco Badia <ricard.zarco@c5s203pc212.fib.upc.es>
2018-04-20 18:02:53 +0200

    Memes labo 4 on the way

---
+++ Labo4/Sweep2/HRC04.h
@@ -0,0 +1,21 @@
+#ifndef HRC04_H
+#define HRC04_H
+
+#include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+/* Clase destinada al uso del sensor
+ * HRC04, es capaz de retornar la distancia
+ * a la cual se encuentra un objecto de el */
+
+class HRC04{
+public:
+	HRC04();
+	~HRC04();
+	double getDistancia();	
+  
+};
+
+#endif
--- Labo4/Sweep2/Sweep2.ino
+++ Labo4/Sweep2/Sweep2.ino
@@ -1,17 +1,22 @@
 #include <Servo.h>
+#include "HRC04.h"
 
-#define FASTASFUCK 30
-#define NOTSOFAST 15
-#define SLOWASFUCK 5
+#define FASTASFUCK 120
+#define NOTSOFAST 60
+#define SLOWASFUCK 15
+
+//TODO: EXPLAIN CODE AND CALCULATIONS
 
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
+
+HRC04* dist_sensor;
 
 
 //Funció que fa rotació a tants Degree Per Second com indiquem 
 void spin2Win(int dps){
   
-  int retraso = (int) 1000.0/dps; 
+  int retraso = 1000/dps; 
   int pos = 0;    // variable to store the servo position
 
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
@@ -25,12 +30,31 @@ void spin2Win(int dps){
   }
 }
 
+//Function to map distance from the sun and beyond and map it to the servo
+void distanceToPosition(){
+
+  int mm = dist_sensor->getDistancia();;
+  /*for(int i = 0; i < 5; ++i){
+    mm += dist_sensor->getDistancia();
+  }*/
+  
+  mm = mm/5;
+  Serial.println(mm);
+
+  if(mm > 180) mm = 180;
+  myservo.write(mm);  
+}
+
 void setup() {
   Serial.begin(9600);
   myservo.attach(9, 700, 2400);  // Custom min and max
+
+  dist_sensor = new HRC04();
+  
 }
 
 void loop() {
-  spin2Win(30);
+  //spin2Win(180);
+  distanceToPosition();
 }
 
