5a315a9e448d9f56179f3c32b0e1997835c005eb
Marc43 <marcdominguezr@gmail.com>
2018-06-10 10:42:13 +0200

    Tracer porteado a la version antigua

--- Project/rto_1/rto_1.ino
+++ Project/rto_1/rto_1.ino
@@ -10,18 +10,10 @@
 #define trigPin 11
 #define echoPin 12
 #define TASKS 1 //Numero de tareas diferentes el Tracer que hay en el RT
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 8192
 
 /////////////INSANE GLOBAL VARIABLES HERE///////
 double distance;
-
-/*char state [TASKS]; //Aqui almacenan su estado
-
-typedef enum state_t {
-  READY,
-  RUNNING,
-  BLOCKED
-};*/
 
 int tracerfile_fd;
 
@@ -61,13 +53,35 @@ void taskActuator(void * pvParameters){
 
 void taskTracer(void * pvParameters) {
   
-      for (;;) {
-        Serial.println("eeeeee");
-        static char buff [BUFFER_SIZE];
-        
-        vTaskList(buff); 
+	  /* Esta tarea es encargada de hacer la traza del sistema   	*/
+	  /* durante los 2s de ejecucion, podemos asumir que siempre 	*/
+	  /* cumplira el deadline dado que tendra la mayor prioridad 	*/
+      /* posible, seguramente en un sistema 100% real, se quitaria.	*/
+
+      int array_size;
+      TaskStatus_t* status_array;
+
+	  int count = 0;
+      
+	  char buff [BUFFER_SIZE];
+
+	  for (;;) {
+		array_size = uxCurrentNumberOfTask();
+		status_array = pvPortMalloc(sizeof(TaskStatus)*array_size); //Dinamicamente pedimos memoria;
+
+    	array_size = uxTaskGetSystemState(status_array, array_size, NULL); //Puntero al vector de estructura, size que nos gustaria, no nos hace falta el tiempo de boot;    
+     
+		int i;
+		sprintf(&buff, "/***time: %d ms\n***\\", count*25); //tiempo en el que se tomó esta parte de la traza
+		for (i=0; i<array_size; i++)
+			sprintf(&buff, "%s\t%d\n", array_size[i].pcTaskName, array_size[i].uxCurrentPriority);
+		
+		sprintf(&buff, "\\******************/"); //seguramente quede un poco feo xdddddddd
+		//Hay que gestionar con un timer cuando pasan los 2 s para hacer un write del buffer en un archivo ...
 
         Serial.println(buff);
+		++count;
+		free(status_array);
       }
       
 }
